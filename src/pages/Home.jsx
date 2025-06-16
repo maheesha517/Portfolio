a50860cf45a93a12c6356bedaba0689bb2859b90
Mathangadeerage Maheesha <127947717+maheesha517@users.noreply.github.com>
2025-06-16 15:07:18 +0530

    Projects

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import { Navbar } from "../components/Navbar";
 import { HeroSection } from "../components/HeroSection";
 import { AboutSection} from "../components/AboutSection";
 import { SkillsSection } from "../components/SkillsSection";
+import { ProjectSection } from "../components/ProjectSection";
+import { ContactSection } from "../components/ContactSection";
 
 
 export const Home =() => {
@@ -17,6 +19,8 @@ return(
       <HeroSection/>
       <AboutSection/>
       <SkillsSection/>
+      <ProjectSection/>
+      <ContactSection/>
    </main>
  </div>
 );
